7714b92966ffd37bfdd592b7b6cf01fc10683831
Roman Guseinov <gromcase@gmail.com>
2017-10-07 20:53:05 +0800

    Added react-signals-plot component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import ReactSignalsPlot from 'react-signals-plot';
 
 export default class Main extends React.Component {
   render() {
     return (
-      <div>Main Component</div>
+      <div>
+        <div>Component Example:</div>
+        <ReactSignalsPlot />
+      </div>
     );
   }
 }
